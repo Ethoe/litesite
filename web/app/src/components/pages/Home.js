788f4a11e978eb0dc09cd9efe30c13854754e9de
ethoe <ryanouttrim@gmail.com>
2023-07-27 14:54:43 -0700

    setup api client

---
+++ web/app/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../../assets/styles/home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Website</h1>
+            <p>
+                Hey I'm Ryan, a software developer. I'm currently trying to learn React and frontend in general.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ web/app/src/services/apiClient.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const baseURL = 'https://ethoe.dev/api'; // Replace with the URL of your Golang backend API
+
+const apiClient = axios.create({
+    baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const get = async (url) => {
+    try {
+        const response = await apiClient.get(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+};
+
+export const post = async (url, data) => {
+    try {
+        const response = await apiClient.post(url, data);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating data:', error);
+        throw error;
+    }
+};
+
+export const put = async (url, data) => {
+    try {
+        const response = await apiClient.put(url, data);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating data:', error);
+        throw error;
+    }
+};
+
+export const remove = async (url) => {
+    try {
+        const response = await apiClient.delete(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting data:', error);
+        throw error;
+    }
+};
+
+export default apiClient;
