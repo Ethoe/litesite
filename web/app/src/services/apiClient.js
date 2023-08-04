e0aa39deee4686e52ee7db555572f38c024a7a54
ethoe <ryanouttrim@gmail.com>
2023-08-04 14:22:59 -0700

    set up some env changes

--- web/app/src/services/apiClient.js
+++ web/app/src/services/apiClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 //const baseURL = 'https://ethoe.dev/api'; // Replace with the URL of your Golang backend API
-const baseURL = 'http://localhost:5050/api'
+//const baseURL = 'http://localhost:5050/api'
+const baseURL = process.env.REACT_APP_API_URL || 'https://ethoe.dev/api';
+
 
 const apiClient = axios.create({
     baseURL,
